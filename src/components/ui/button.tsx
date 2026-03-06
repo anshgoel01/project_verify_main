781c17e303474d48cd0135dbebf3da53bba3485d
Anshuman goel <151727993+anshgoel01@users.noreply.github.com>
2026-03-06 11:25:24 +0530

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -46,6 +46,7 @@ Button.displayName = "Button";
 
 export { Button, buttonVariants };
 
+
 // import * as React from "react";
 // import { Slot } from "@radix-ui/react-slot";
 // import { cva, type VariantProps } from "class-variance-authority";

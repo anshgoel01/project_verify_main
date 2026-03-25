d6825f8e73e665eab0476754042a6332fb92b8ab
Anshuman goel <anshg112005@gmail.com>
2026-03-25 10:14:39 +0530

    fix export

--- supabase/functions/admin-export/index.ts
+++ supabase/functions/admin-export/index.ts
@@ -1,4 +1,5 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+// @ts-nocheck
+import { createClient } from "supabase";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
